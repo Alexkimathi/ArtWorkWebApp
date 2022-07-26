1a4eb11279a778faa7b7e626ee80376dea6471aa
alex kimathi <keemalekie@gmail.com>
2022-07-26 18:15:14 +0300

    add modifications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
-import { Route,Routes } from "react-router-dom";
-
+import React,{useEffect, useState} from "react";
+import {Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AllArts from "./pages/AllArts";
+import FavoriteArt from "./pages/FavoriteArt";
+import NewArt from "./pages/NewArt";
+
 
 function App() {
-  return (
-    <>
-    <Navbar />
-    <div>
-    <Routes>
-    <Route/>
-    
-    </Routes>
-   
-    </div>
-    
-    </>
+  const [art , setArt] = useState([])
 
+  useEffect(() =>{
+    fetch ("http://localhost:8000/ArtsWork")
+    .then((response) => response.json())
+    .then((data) => setArt(data))
+  
+},[])  
+  return (
+    <div>
+      <Navbar />
+      
+        <Routes>
+          <Route path="/" element={<AllArts />} />
+          <Route path="favoriteart" element={<FavoriteArt />} />
+          <Route path="newarts" element={<NewArt />} />
+        </Routes>
+  
+    </div>
   );
 }
 
