e465da2786b39d1bdf619534c5a19566a14d118b
alex kimathi <keemalekie@gmail.com>
2022-07-27 18:22:19 +0300

    add the post method in the newart form

--- src/pages/NewArt.js
+++ src/pages/NewArt.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NewArtsForm from "../components/Arts/NewArtsForm";
+import '../App.css'
 
 function NewArt() {
   return (
     <div>
+    <h1 className="new">New Art Form</h1>
     <NewArtsForm />
      
     </div>
